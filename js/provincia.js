2c1620affea9cfef75b4f6d78cc2a890d77cc339
Mario Aguilar <mariocba150@gmail.com>
2024-04-01 01:33:55 -0300

    Se agregan clases a los elementos que se renderizan, se agrega título de la provincia elegida en los elementos a renderizar, y se modifican algunos  nombres de clases

--- js/provincia.js
+++ js/provincia.js
@@ -1,40 +1,58 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const detalleProvincia = document.getElementById("detalle-provincia");
+document.addEventListener('DOMContentLoaded', () => {
+	const detalleProvincia = document.getElementById('provinceDetail');
 
-  //obtenemos el ID de la provincia de la URL
-  const urlParams = new URLSearchParams(window.location.search);
-  const idProvincia = urlParams.get("id");
+	//Obtenemos el ID de la provincia desde la URL
+	const urlParams = new URLSearchParams(window.location.search);
+	const idProvincia = urlParams.get('id');
 
-  //Obtenemos los datos de la provinvia desde el localStorage
-  const datosProvincias = JSON.parse(localStorage.getItem("provincias"));
+	//Obtenemos los datos de la provincia desde el localStorage
+	const datosProvincias = JSON.parse(localStorage.getItem('provincias'));
 
-  //Buscamos la provincia por ID
-  const provinciaSeleccionado = datosProvincias.provincias.find(
-    (provincia) => provincia.id == idProvincia
-  );
+	//Buscamos la provincia por ID
+	const provinciaSeleccionada = datosProvincias.provincias.find(
+		(provincia) => provincia.id == idProvincia
+	);
 
-  if (provinciaSeleccionado) {
-    //Creamos un elemento div para mostrar los detalles de la provincia y lo agregamos al DOM
-    const contenedorDetallesProvincia = document.createElement("div");
-    contenedorDetallesProvincia.classList.add("detalles-provincia");
-    contenedorDetallesProvincia.innerHTML = `
-    <h2>${provinciaSeleccionado.descripcion1}</h2>
-    <img src="${provinciaSeleccionado.imagen1}" alt="${provinciaSeleccionado.descripcion1}">
-    <p>${provinciaSeleccionado.detalle1}</p>
-    <h2>${provinciaSeleccionado.descripcion2}</h2>
-    <img src="${provinciaSeleccionado.imagen2}" alt="${provinciaSeleccionado.descripcion2}">
-    <p>${provinciaSeleccionado.detalle2}</p>
-    <h2>${provinciaSeleccionado.descripcion3}</h2>
-    <img src="${provinciaSeleccionado.imagen3}" alt="${provinciaSeleccionado.descripcion3}">
-    <p>${provinciaSeleccionado.detalle3}</p>
-    <h2>${provinciaSeleccionado.descripcion4}</h2>
-    <img src="${provinciaSeleccionado.imagen4}" alt="${provinciaSeleccionado.descripcion4}">
-    <p>${provinciaSeleccionado.detalle4}</p>
-    <p>Valor: ${provinciaSeleccionado.precio} USD</p>
+	// Renderizado de provincias
+	if (provinciaSeleccionada) {
+		//Creamos un elemento div para mostrar los detalles y lo agregamos al DOM
+		const contenedorDetallesProvincia = document.createElement('div');
+		contenedorDetallesProvincia.classList.add('provinciaSelectedContainer');
+		contenedorDetallesProvincia.innerHTML = `
+      <h2 class="provinciaSelectedName">${provinciaSeleccionada.nombre}</h2>
+      <div class="activity">
+        <h3 class="activityTitle">${provinciaSeleccionada.descripcion1}</h3>
+        <div>
+        <img class="activityImage" src="${provinciaSeleccionada.imagen1}" alt="${provinciaSeleccionada.descripcion1}">
+        </div>
+        <p class="activityDescription">${provinciaSeleccionada.detalle1}</p>
+      </div>
+      <div class="activity">
+        <h3 class="activityTitle">${provinciaSeleccionada.descripcion2}</h3>
+        <div>
+        <img class="activityImage" src="${provinciaSeleccionada.imagen2}" alt="${provinciaSeleccionada.descripcion2}">
+        </div>
+        <p class="activityDescription">${provinciaSeleccionada.detalle2}</p>
+      </div>
+      <div class="activity">
+        <h3 class="activityTitle">${provinciaSeleccionada.descripcion3}</h3>
+        <div>
+        <img class="activityImage" src="${provinciaSeleccionada.imagen3}" alt="${provinciaSeleccionada.descripcion3}">
+        </div>
+        <p class="activityDescription">${provinciaSeleccionada.detalle3}</p>
+      </div>
+      <div class="activity">
+        <h3 class="activityTitle">${provinciaSeleccionada.descripcion4}</h3>
+        <div>
+        <img class="activityImage" src="${provinciaSeleccionada.imagen4}" alt="${provinciaSeleccionada.descripcion4}">
+        </div>
+        <p class="activityDescription">${provinciaSeleccionada.detalle4}</p>
+      </div>
+      <div class="activity">
+      <h4 class="activityPrice">Valor: ${provinciaSeleccionada.precio} USD</h4>
+      </div>
     `;
-    //agregar a pagina principal
-    detalleProvincia.appendChild(contenedorDetallesProvincia);
-    
-  }
+		//agregar a pagina principal
+		detalleProvincia.appendChild(contenedorDetallesProvincia);
+	}
 });
-
