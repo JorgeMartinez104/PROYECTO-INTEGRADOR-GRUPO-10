8f291988352d5b10c1ffd24c8b04fa92913902f0
cintiacas <cintiscas@hotmail.com>
2024-04-13 10:18:38 -0300

    deshacer cambios

--- js/scriptForm.js
+++ js/scriptForm.js
@@ -1,66 +1,11 @@
-//const formulario = document.querySelector("form");
-const form = document.getElementById("form");
+const formulario = document.querySelector("form");
 const borrarConfig = document.querySelector("#borrarConfig");
-const nameForm = document.getElementById("nombre");
-const lastNameForm = document.getElementById("apellido");
-const emailForm = document.getElementById("mail");
-const phoneForm = document.getElementById("telefono");
-const listInputs = document.querySelectorAll(".form-input");
-const resetBtn = document.getElementById('btn-clear');
-const textArea = document.getElementById("consulta");
-const inputs = form.getElementsByTagName('input')
-const select = form.getElementsByTagName('select')[0]
-const radio = document.getElementsByName("transporte");
-
 
 // Escuchador de evento para cuando se envie el formulario
-form.addEventListener("submit", (event) => {
+formulario.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  let condicion = validateForm();
-
-  if (condicion) {
-    guardarConfig();
-  }
+  guardarConfig();
 });
-
-function validateForm() {
-  let testEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-
-  listInputs.forEach((element) =>
-  {
-    element.lastElementChild.innerHTML = "";
-  });
-
-  if(nameForm.value.trim() == "")
-  {
-    showMessError("nombre", "*Nombre no válido.");
-    condicion = false;
-  }
-  console.log("falso")
-  if(lastNameForm.value.trim() == "")
-  {
-    showMessError("apellido", "*Apellido no válido.");
-    condicion = false;
-  }
-
-  if(phoneForm.value.length < 10 || phoneForm.value.trim() == "")
-  {
-    showMessError("telefono", "*Teléfono no válido.");
-    condicion = false;
-  }
-
-  if(!testEmail.test(emailForm.value)){
-    showMessError("mail", "*Email no válido.");
-    condicion = false;
-  }
-}
-function showMessError(classInput, mess)
-{
-  let eleName = document.querySelector(`.${classInput}`);
-  eleName.lastElementChild.innerHTML = mess;
-}
-
 // Escuchador de evento para cuando se haga click en borrar configuracion
 borrarConfig.addEventListener("click", limpiarConfig);
 // Escuchador de evento para que cuando se cargue la página, se cargue la info del local storage en caso que exista
@@ -92,32 +37,3 @@ function limpiarConfig() {
   document.querySelector("#username").value = "";
   document.body.className = ``;
 }
-
-const reset = (e) => {
-
-  e.preventDefault()
-
-  var ele = radio;
-
-  for (let input of inputs)
-  input.value = ''
-
-  select.value = 'bs-as'
-  
-  textArea.value = "";
-
-  ele[0].checked = true;
-
-  for(var i=1;i<ele.length;i++)
-  { 
-      ele[i].checked = false;
-  }
-
-  listInputs.forEach((element) =>
-  {
-    element.lastElementChild.innerHTML = "";
-  });
-
-}
-
-resetBtn.addEventListener('click', reset)
